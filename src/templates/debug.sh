4eb800dc8cc25c6a086accb92ea309a7b9554d4b
Michael Warr <mtw_w@outlook.com>
2019-03-21 15:23:36 +0000

    debug.sh - docker rm -f

--- src/templates/debug.sh
+++ src/templates/debug.sh
@@ -4,7 +4,7 @@
 
 debug_name="debug-$name"
 docker stop "$debug_name" > /dev/null 2>&1
-docker rm "$debug_name" > /dev/null 2>&1
+docker rm -f "$debug_name" > /dev/null 2>&1
 
 docker run -it \
   --name="$debug_name" \
